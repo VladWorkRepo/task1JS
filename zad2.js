8ae3ad6ba6ab0259bf3f6f60b4c71c18c5df589b
Vlad <vlad.herasymovych@tutore.eu>
2021-12-16 13:31:02 +0100

    zad2 verify and comments

--- zad2.js
+++ zad2.js
@@ -1,17 +1,29 @@
+//validate password
 function validatePassword(password) {
-    if(typeof password ==="string") {
-        let specialChars = /[1234567890]/;
-        if(password.length >=3 && password.length <=10) {
-            if(specialChars.test(password) && password.includes("#") || password.includes("@") || password.includes("!")) {
+    if(typeof password ==="string") { //check if the input password is string
+        let specialChars = /[1234567890]/; //implement number values
+        if(password.length >=3 && password.length <=10) { //check how long is the input pass
+            if(specialChars.test(password) && password.includes("#") || password.includes("@") || password.includes("!")) { //check special characters
                 return true;
             } else {
                 return false;
             }
         } else {
-            console.log("Your password must be between 3 and 10 chars");
+            return false;
         }
     } else {
         console.log(`${password} - this value not a string!`);
     }
 }
 
+ function verify(input, goal) {
+    if (input === goal) {
+      console.log('Gratulacje!');
+    } else {
+      console.log(`Niestety, oczekiwano - ${goal}, otrzymano - ${input}`);
+    }
+  }
+  
+  verify(validatePassword(''), false);
+  verify(validatePassword('lol'), false);
+  verify(validatePassword('ToDziala1#'), true);
