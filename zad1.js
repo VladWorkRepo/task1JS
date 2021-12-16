6f42164bf898a6baf095bf31a164b6e30e020e56
Vlad <vlad.herasymovych@tutore.eu>
2021-12-16 12:51:51 +0100

    comments and call function

--- zad1.js
+++ zad1.js
@@ -1,9 +1,9 @@
+// create a reverse function
 function reverseMe(input) {
-
-  if(typeof input === 'string') {
-    let split = input.split("");
-    let reverseArr = split.reverse();
-    let joinInput = reverseArr.join("");
+  if(typeof input === 'string') { //check if the input value is string
+    let split = input.split(""); //split the input value
+    let reverseArr = split.reverse(); //reverse splited input
+    let joinInput = reverseArr.join(""); // join each letter to one word
     return joinInput;
   } else {
     console.log(`${input} - this value not a string!`);
@@ -11,6 +11,7 @@ function reverseMe(input) {
     
   }
     
+  // verify function
   function verify(input, goal) {
     if (input === goal) {
       console.log('Gratulacje!');
@@ -18,3 +19,8 @@ function reverseMe(input) {
       console.log(`Niestety, oczekiwano - ${goal}, otrzymano - ${input}`);
     }
   }
+
+  verify(reverseMe('a'), 'a');
+  verify(reverseMe('abc'), 'cba');
+  verify(reverseMe('Przeprogramowani'), 'inawomargorpezrP');
+  verify(reverseMe('Brawo!'), '!owarB');
