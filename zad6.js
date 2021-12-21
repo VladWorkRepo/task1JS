00fd31c0fea02f8c8f72e4728b17069aebf585e1
Vlad <vlad.herasymovych@tutore.eu>
2021-12-21 10:37:05 +0100

    predicate function and verify

--- zad6.js
+++ zad6.js
@@ -1,27 +1,36 @@
 function applyPredicate(elements, predicate) {
-    predicate = x => {
-        for(let i=0; i<elements.length; i++) {
-            if(elements[i])
+    let tab = [];
+    for(let i in elements) {
+        if(predicate(elements[i])) {
+            tab.push(elements[i]);
         }
-    };
+    }
+    return tab;
+}
+
+function verify(input, goal) {
+    const inputAsString = JSON.stringify(input);
+    const goalAsString = JSON.stringify(goal);
+  
+    if (inputAsString === goalAsString) {
+      console.log('Gratulacje!');
+    } else {
+      console.log(`Niestety, oczekiwano - ${goalAsString}, otrzymano - ${inputAsString}`);
+    }
   }
-
-//console.log(applyPredicate([1, 2, 3], element => element > 2));
-
-const test1 = element => element > 2;
-console.log(test1(3));
-
-// function verify(input, goal) {
-//     const inputAsString = JSON.stringify(input);
-//     const goalAsString = JSON.stringify(goal);
   
-//     if (inputAsString === goalAsString) {
-//       console.log('Gratulacje!');
-//     } else {
-//       console.log(`Niestety, oczekiwano - ${goalAsString}, otrzymano - ${inputAsString}`);
+  verify(applyPredicate([1, 2, 3], element => element > 2), [3]);
+  verify(applyPredicate(['a', 'b', 'c'], element => ['b', 'c'].includes(element)), ['b', 'c']);
+  verify(applyPredicate(['x', 'y'], element => element === 'z'), []);
+
+
+// let elements = ['a', 'b', 'c'];
+// let test=[];
+// for(let i in elements) {
+//     const predicate = element => ['b', 'c'].includes(element);
+//     if(predicate(elements[i])) {
+//         test.push(elements[i]);
 //     }
-//   }
-  
-//   verify(applyPredicate([1, 2, 3], element => element > 2), [3]);
-//   verify(applyPredicate(['a', 'b', 'c'], element => ['b', 'c'].includes(element)), ['b', 'c']);
-//   verify(applyPredicate(['x', 'y'], element => element === 'z'), []);
+// }
+
+// console.log(test);
