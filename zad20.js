7de58432d782703b1f28dc39d9d8595429b8b31c
Vlad <vlad.herasymovych@tutore.eu>
2021-12-25 18:41:13 +0100

    zad20 flattenArray function

--- zad20.js
+++ zad20.js
@@ -1,18 +1,35 @@
 function flattenArray(deepArray) {
-    deepArray = deepArray.split(", ");
-    console.log(deepArray);
+    let newArr = deepArray.flat();
+    for(let i in newArr) {
+        const e = newArr[i];
+
+        let lastIndex = newArr.lastIndexOf(e);
+        let index = newArr.indexOf(e);
+
+        if(index !== lastIndex) {
+            newArr.splice(lastIndex,1);
+        }
+    }
+
+    for(let i in newArr) {
+        if(newArr[i] == null || newArr[i] == undefined) {
+            newArr.splice(i,1);
+        }
+    }
+
+    return newArr;
 }
 
-console.log(flattenArray([4, [3, 2, undefined, 1], [1, 4, null, 5]]));
+//console.log(flattenArray([null, [1, 2, 3], [null, undefined]]));
 
-// function verify(input, goal) {
-//   input = Array.isArray(input) ? input.join(', ') : input;
-//   if (input == goal) {
-//     console.log('Gratulacje!');
-//   } else {
-//     console.log(`Niestety, oczekiwano - ${goal}, otrzymano - ${input}`);
-//   }
-// }
+function verify(input, goal) {
+  input = Array.isArray(input) ? input.join(', ') : input;
+  if (input == goal) {
+    console.log('Gratulacje!');
+  } else {
+    console.log(`Niestety, oczekiwano - ${goal}, otrzymano - ${input}`);
+  }
+}
 
-// verify(flattenArray([4, [3, 2, undefined, 1], [1, 4, null, 5]]), "4, 3, 2, 1, 5");
-// verify(flattenArray([null, [1, 2, 3], [null, undefined]]), "1, 2, 3");
+verify(flattenArray([4, [3, 2, undefined, 1], [1, 4, null, 5]]), "4, 3, 2, 1, 5");
+verify(flattenArray([null, [1, 2, 3], [null, undefined]]), "1, 2, 3");
