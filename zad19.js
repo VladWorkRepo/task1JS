1361d7725e882edbcd72d15a3a52c9db58f7464f
Vlad <vlad.herasymovych@tutore.eu>
2021-12-26 14:20:39 +0100

    zad19 checkBrackets function

--- zad19.js
+++ zad19.js
@@ -1,22 +1,57 @@
 function checkBrackets(stringWithBrackets) {
-    
+    const leftBraces = [];
+  
+    for (let i = 0; i < stringWithBrackets.length; i++) {
+      const char = stringWithBrackets[i];
+  
+      switch (Brace(char)) {
+        case 'L':
+          leftBraces.push(char);
+          break;
+        case 'R':
+          if (!Match(leftBraces.pop() + char)) {
+            return false;
+          }
+          break;
+      }
+    }
+  
+    return leftBraces.length === 0; 
 }
 
-console.log(checkBrackets("[{()}]"));
+function Brace(c) {
+    switch (c) {
+        case ')': case '}': case ']':
+        return 'R';
+        case '(': case '{': case '[':
+        return 'L';
+        default:
+        return ''; 
+    }
+}
+  
+function Match(brackets) {
+    switch (brackets) {
+        case '()': case '{}': case '[]':
+        return true;
+        default:
+        return false;
+    }
+}
 
-// function verify(input, goal) {
-//   input = Array.isArray(input) ? `[${input.join(', ')}]` : input;
-//   if (input == goal) {
-//     console.log('Gratulacje!');
-//   } else {
-//     console.log(`Niestety, oczekiwano - ${goal}, otrzymano - ${input}`);
-//   }
-// }
+function verify(input, goal) {
+  input = Array.isArray(input) ? `[${input.join(', ')}]` : input;
+  if (input == goal) {
+    console.log('Gratulacje!');
+  } else {
+    console.log(`Niestety, oczekiwano - ${goal}, otrzymano - ${input}`);
+  }
+}
 
-// verify(checkBrackets("[{()}]"), true);
-// verify(checkBrackets("[{]}"), false);
-// verify(checkBrackets("()[{}]"), true);
-// verify(checkBrackets("{[(]}}"), false);
-// verify(checkBrackets("[{()]}"), false);
-// verify(checkBrackets("[]{})("), false);
-// verify(checkBrackets("()([{})]"), false);
+verify(checkBrackets("[{()}]"), true);
+verify(checkBrackets("[{]}"), false);
+verify(checkBrackets("()[{}]"), true);
+verify(checkBrackets("{[(]}}"), false);
+verify(checkBrackets("[{()]}"), false);
+verify(checkBrackets("[]{})("), false);
+verify(checkBrackets("()([{})]"), false);
